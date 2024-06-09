e7c3f01d94833020ae2895d5f2114e23ee35f160
Yashasvi9849 <yashureddy9849@gmail.com>
2024-06-08 21:11:55 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Area{
+    public static void main(String args[]){
+         double pi, a,r;
+          pi = 3.14F;
+          r=10.8;
+          a = pi*r*r;
+          System.out.println("Area of circle is " +a);
+
+    }
+}

0ea5298a53eb6a341ed8f64de0cb8bb5a5dd048e
Yashasvi9849 <yashureddy9849@gmail.com>
2024-06-08 21:21:08 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class IfSample{
-    public static void main(String args []){
-        int a,b;
-        a=10;
-        b=20;
-        //if loop
-        if(a < b) System.out.println("a is less than b ");
-        a=a*2;
-        if(a==b) System.out.println("a is equal to b ");
-        a=a*2 ;
-       if(a>b) System.out.println("a is greater than b ");
+class ArrayAverage{
+    public static void main(String args[]){
+        double nums[] = {10.11,12.23,13.45,15,5,18.75};
+        double res = 0;
+        double avg;
+        int i;
+        for(i=0;i<6;i++)
+            res = res+nums[i];
+        System.out.println("The average is " +(res/6));
 
 
 

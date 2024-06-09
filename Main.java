e6209ffacbbfd188709075984ce30ce5f15bae76
Yashasvi9849 <yashureddy9849@gmail.com>
2024-06-08 21:14:21 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
-class Area{
-    public static void main(String args[]){
-         double pi, a,r;
-          pi = 3.14F;
-          r=10.8;
-          a = pi*r*r;
-          System.out.println("Area of circle is " +a);
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class IfSample{
+    public static void main(String args []){
+        int a,b;
+        a=10;
+        b=20;
+        //if loop
+        if(a < b) System.out.println("a is less than b ");
+        a=a*2;
+        if(a==b) System.out.println("a is equal to b ");
+        a=a*2 ;
+       if(a>b) System.out.println("a is greater than b ");
+
+
 
     }
 }
